6cf0943bd0d5123e12f5850e23ab78943d2f72af
solenrri <56745468+solenrri@users.noreply.github.com>
2025-06-01 15:27:55 -0300

    dibujos.h

--- dibujos.h
+++ dibujos.h
@@ -2,10 +2,14 @@
 #define DIBUJOS_H_INCLUDED
 #include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #define TAM_PIXEL 5
 #define PIXELES_X_LADO 8
 #define TAM_GRILLA 8
 #define PX_PADDING 4
+#define DIM 8
+#define MINA -1
+#define VACIA 0
 
 #define G 0 // Gris
 #define C 1 // Claro
@@ -27,6 +31,8 @@ void dibujar(SDL_Window *ventana, SDL_Renderer *renderer, const int[][PIXELES_X_
 void dibujarCampo(SDL_Window *ventana, SDL_Renderer *renderer, const int[][PIXELES_X_LADO]);
 void dibujarSegunCorresponda(SDL_Window *ventana, SDL_Renderer *renderer, int x, int y);
 void borrarPantalla(SDL_Window *ventana, SDL_Renderer *renderer);
-void colocarMinas();
+void  inicializar_matriz(int matriz[DIM][DIM]);
+void colocarMinas(int mat[DIM][DIM]);
+void mostrar_matriz(int mat[DIM][DIM]);
 
 #endif // DIBUJOS_H_INCLUDED
