33a5dc66f117338bfbee9f554350a6e50d022ee0
solenrri <56745468+solenrri@users.noreply.github.com>
2025-06-09 23:57:58 -0300

    Create configuracion.h

---
+++ configuracion.h
@@ -0,0 +1,19 @@
+#ifndef CONFIGURACION_H_INCLUDED
+#define CONFIGURACION_H_INCLUDED
+
+#define TODO_OK 1
+#define ERROR_ARCH  2
+#define SIN_MEM 3
+
+typedef struct
+{
+    int dimension;
+    char cantidad_minas[4];
+}t_parametria;
+
+int leer_Archivo(t_parametria* param);
+int es_porcentaje(char* cad);
+int valor_de_las_minas(t_parametria* param);
+
+
+#endif // CONFIGURACION_H_INCLUDED
